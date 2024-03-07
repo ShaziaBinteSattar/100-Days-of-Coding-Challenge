a03c5840d44a01c6aa3b3f9177ad7bfc661e4c59
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-08 01:35:10 +0500

    Add files via upload

---
+++ Question 4: Famous Quote/Famous_Quote.js
@@ -0,0 +1,4 @@
+// Famous Quote: Program to Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//"Albert Einstein once said, “A person who never made a mistake never tried anything new.”//
+var personName = "Nelson Mandella";
+console.log("".concat(personName, " once said ,\"the greatest glory in living lies not in never falling, but in rising every time we fall.\""));
