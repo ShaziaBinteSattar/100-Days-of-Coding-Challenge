2e3c2d89a942cf722e58424686b7c500d26a8b47
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-08 00:55:15 +0500

    Add files via upload

---
+++ Question 2: Personal Message/Personal_Message.ts
@@ -0,0 +1,7 @@
+//typeScript program: Personal message; Store a person's name in a variable and print a message to them, like "Hello Shazia Binte Abdus sattar, would you like to learn some Typescript today?"//
+import { clearScreenDown } from "readline";
+//this saves the name variable as string//
+let personName: string = "Shazia Binte Abdus Sattar";
+//This shows the print output//
+console.log(`"Hello ${personName},Would you like to learn some Typescript today?"`);
+
