46f746851a6561f88e55d3040dfd503ae6289336
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-08 01:59:12 +0500

    Add files via upload

---
+++ Question 5: Famous Quote2/Famous_Quote2.js
@@ -0,0 +1,7 @@
+"use strict";
+// Famous Quote 2:program to Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+//this saves the name variable as string//
+let Famous_Person = "Maya Angelou";
+let message = `${Famous_Person} once said," I have learned that people will forget what you said, people will forget what you did, but \nPeolpe will NEVER forget how you made them feel"`;
+//This shows the print output//
+console.log(message);
