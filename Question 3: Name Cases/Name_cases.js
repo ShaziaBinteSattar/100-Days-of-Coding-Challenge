805992c7f60e6103087a8f2c7149f181692e8c5e
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-03-08 00:58:01 +0500

    Add files via upload

---
+++ Question 3: Name Cases/Name_cases.js
@@ -0,0 +1,8 @@
+"use strict";
+//Names_cases; store a person's name in avariable, and teh print that person's name in lower case, upper case and title case"//node_modules
+//this saves the name variable as string//
+let personName = "Shazia Binte Abdus Sattar";
+//This shows the print output//
+console.log("Lowercase :" + personName.toLowerCase()); //lower case//
+console.log("Uppercase :" + personName.toUpperCase()); //Upper case//
+console.log("Titlecase :" + personName.replace(/\b\w/g, c => c.toUpperCase())); //Title case//
