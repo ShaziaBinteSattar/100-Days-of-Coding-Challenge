95681a442c32ac8911658dddfb6163f485a0312e
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 17:11:16 +0500

    Add files via upload

---
+++ Question_09_Favorite_Number/Favorite_Number.js
@@ -0,0 +1,5 @@
+"use strict";
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message
+// revealing your favorite number. Print that message.
+let favoriteNumber = 7;
+console.log('My favorite number is  :', favoriteNumber);
---
+++ Question_09_Favorite_Number/Favorite_Number.ts
@@ -0,0 +1,12 @@
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message
+// revealing your favorite number. Print that message.
+//this will store my favorite number in a variable
+
+let favoriteNumber: number = 7;
+
+//this will create a message revealing my favorite number
+let message: string = `My favorite number is ${favoriteNumber}`;
+
+//this will print the message
+console.log(message);
+
