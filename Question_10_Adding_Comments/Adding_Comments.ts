1ca0666eba0a983c01b46af9c86f6b106c6ae717
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 17:24:49 +0500

    Add files via upload

---
+++ Question_10_Adding_Comments/Adding_Comments.ts
@@ -0,0 +1,18 @@
+//Shazia Binte Abdus Sattar, 20th of july,2024.
+//Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+// If the programs are straightforward at this point, just add your name and the current date at the top of 
+//each program file. Then, write one sentence describing what the program does.
+
+
+// This program prints a personal message
+let myName: string = "Shazia Binte Abdus Sattar";
+
+console.log(`Hello ${myName}, would you like to learn some TypeScript today?`);
+
+
+
+
+
+
+
+
