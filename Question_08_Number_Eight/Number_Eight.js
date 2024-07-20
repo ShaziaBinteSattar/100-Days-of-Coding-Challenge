aae5ca2ce9db4d49f4b0f229945f5669fb09cc98
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 16:58:31 +0500

    Add files via upload

---
+++ Question_08_Number_Eight/Number_Eight.js
@@ -0,0 +1,12 @@
+"use strict";
+//Question 8: You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+// Addition
+console.log("Addition operation 5 + 3 : that gives output as number 8:", 5 + 3);
+// Addition
+console.log("Addition operation 4 + 4 : that gives output as number 8:", 4 + 4);
+// Addition
+console.log("Addition operation 6 + 2 : that gives output as number 8:", 6 + 2);
+// Addition
+console.log("Addition operation 1 + 7 : that gives output as number 8:", 1 + 7);
