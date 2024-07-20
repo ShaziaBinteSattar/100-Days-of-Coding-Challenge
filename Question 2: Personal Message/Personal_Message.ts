ce894408fded66b22832a31aa07a872f31214104
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 15:32:37 +0500

    Update Personal_Message.ts

--- Question 2: Personal Message/Personal_Message.ts
+++ Question 2: Personal Message/Personal_Message.ts
@@ -1,4 +1,4 @@
-//typeScript program: Personal message; Store a person's name in a variable and print a message to them, like "Hello Shazia Binte Abdus sattar, would you like to learn some Typescript today?"//
+//Q2:typeScript program: Personal message; Store a person's name in a variable and print a message to them, like "Hello Shazia Binte Abdus sattar, would you like to learn some Typescript today?"//
 
 //this saves the name variable as string//
 let personName: string = "Shazia Binte Abdus Sattar";
