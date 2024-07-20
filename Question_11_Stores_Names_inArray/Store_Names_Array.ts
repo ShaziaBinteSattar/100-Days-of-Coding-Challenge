2e4f8ee6d9188678490a66f3b4c1f4dcb1a60582
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 17:44:28 +0500

    Add files via upload

---
+++ Question_11_Stores_Names_inArray/Store_Names_Array.ts
@@ -0,0 +1,13 @@
+//Shazia Binte Abdus Sattar, 20th of July,2024.
+
+//Question 11: Names: Store the names of a few of your friends in an array called names. Print each 
+//person’s name by accessing each element in the list, one at a time.
+
+//this will create array of friends name
+let names:string[] =["Shahnaz","Shazia","Shumaila","Sana", "Sadia"];
+
+//this will print person’s name by accessing each element in the list, one at a time.
+for (let  i=0; i<names.length; i++){
+    console.log("In my friends list Names, one of my friends is:", names[i]);
+    
+}
