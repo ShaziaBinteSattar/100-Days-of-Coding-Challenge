bf9c882aef8b2139f42abe3d3a90440efe59c18b
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 16:50:19 +0500

    Delete Question 2: Personal Message directory

--- Question 2: Personal Message/Personal_Message.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//this saves the name variable as string//
-let personName = "Shazia Binte Abdus Sattar";
-//This shows the print output//
-console.log(`"Hello ${personName},Would you like to learn some Typescript today?"`);
--- Question 2: Personal Message/Personal_Message.ts
+++
@@ -1,7 +0,0 @@
-//Q2:typeScript program: Personal message; Store a person's name in a variable and print a message to them, like "Hello Shazia Binte Abdus sattar, would you like to learn some Typescript today?"//
-
-//this saves the name variable as string//
-let personName: string = "Shazia Binte Abdus Sattar";
-//This shows the print output//
-console.log(`"Hello ${personName},Would you like to learn some Typescript today?"`);
-
