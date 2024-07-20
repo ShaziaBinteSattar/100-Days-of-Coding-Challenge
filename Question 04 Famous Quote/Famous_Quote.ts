24a2640e4333c3fadbc18be4307b342a9d35a5ec
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 15:49:45 +0500

    Add files via upload

---
+++ Question 04 Famous Quote/Famous_Quote.ts
@@ -0,0 +1,7 @@
+// Famous Quote: Program to Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//"Albert Einstein once said, “A person who never made a mistake never tried anything new.”//
+//this saves the name variable as string//
+let personName :string = "Nelson Mandella";  
+//This shows the print output//
+console.log(`${personName} once said ,"the greatest glory in living lies not in never falling, but in rising every time we fall."`);
+
