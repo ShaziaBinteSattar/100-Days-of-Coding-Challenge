822537a45eac6f7adfee54228d8da32ac1097346
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-20 15:30:21 +0500

    Add files via upload

---
+++ Question 2: Personal Message/Personal_Message.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//this saves the name variable as string//
+let personName = "Shazia Binte Abdus Sattar";
+//This shows the print output//
+console.log(`"Hello ${personName},Would you like to learn some Typescript today?"`);
